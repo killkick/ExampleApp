9615944591157d229e3a18ef0c0ead935a4040a4
Yehor <yehor@platform500.com>
2025-08-28 12:43:00 +0200

    [pp-2750] feat: add invoice creation logic in InvoiceController

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,8 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
+use Illuminate\Support\Facades\DB;
+
 class InvoiceController extends Controller
 {
+    public function store()
+    {
+        Invoice::query()->create();
+        return response()->json(['message' => 'Invoice created'], 201);
+    }
 
 
 }
