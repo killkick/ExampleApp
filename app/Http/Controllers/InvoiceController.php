007c880bc987e6f4502f2a492dd7e06ef2a03c72
Yehor <yehor@platform500.com>
2025-08-28 12:44:04 +0200

    [pp-2751] fix: correct success message typo in InvoiceController

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -10,7 +10,7 @@ class InvoiceController extends Controller
     public function store()
     {
         Invoice::query()->create();
-        return response()->json(['message' => 'Invoice created'], 201);
+        return response()->json(['message' => 'Invoice created successfully'], 201);
     }
 
 
